8d34c2e86e3385969d45143dad88a4c6297b360d
Seonghong Min <sh.min@sk.com>
2023-03-29 15:44:42 +0900

    ibft2-3node-test 추가

---
+++ playground/kubectl/quorum-besu/ibft2-3node-test/deploy.sh
@@ -0,0 +1,7 @@
+kubectl apply -f namespace/
+kubectl apply -f secrets/
+kubectl apply -f configmap/
+kubectl apply -f services/
+kubectl apply -f not_yet/
+kubectl apply -f deployments/
+kubectl apply -f statefulsets/
---
+++ playground/kubectl/quorum-besu/ibft2-3node-test/remove.sh
@@ -0,0 +1,9 @@
+
+kubectl delete -f statefulsets/
+
+kubectl delete -f deployments/
+kubectl delete -f not_yet/
+kubectl delete -f secrets/
+kubectl delete -f configmap/
+kubectl delete -f services/
+kubectl delete -f namespace/
