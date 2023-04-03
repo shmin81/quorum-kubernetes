4df1c44d942046a5cfc9981a74289f3981e1a367
Seonghong Min <sh.min@sk.com>
2023-04-03 17:41:23 +0900

    add blockscout extensions

--- playground/kubectl/quorum-besu/ibft2-3node-test/deploy.sh
+++ playground/kubectl/quorum-besu/ibft2-3node-test/deploy.sh
@@ -6,6 +6,13 @@ kubectl apply -f not_yet/
 kubectl apply -f deployments/
 kubectl apply -f statefulsets/
 
-#
+# kubectl apply -f blockscout/smart-contract-verifier-deployment.yaml
+# kubectl apply -f blockscout/visualizer-deployment.yaml
+# kubectl apply -f blockscout/sig-provider-deployment.yaml
+# waiting ... 
 # kubectl apply -f blockscout/blockscout-deployment.yaml
+
 # kubectl delete -f blockscout/blockscout-deployment.yaml
+# kubectl delete -f blockscout/smart-contract-verifier-deployment.yaml
+# kubectl delete -f blockscout/visualizer-deployment.yaml
+# kubectl delete -f blockscout/sig-provider-deployment.yaml
