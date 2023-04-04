09e89d4e87ae812569c24a01db2fcc8ff701a113
Seonghong Min <sh.min@sk.com>
2023-04-04 19:52:57 +0900

    update blockscout

--- playground/kubectl/quorum-besu/ibft2-3node-test/remove.sh
+++ playground/kubectl/quorum-besu/ibft2-3node-test/remove.sh
@@ -1,8 +1,13 @@
 
+kubectl delete -f blockscout_statefulsets/
+kubectl delete -f blockscout_deployments/
+kubectl delete -f blockscout_services/
+kubectl delete -f blockscout_configmap/
+
 kubectl delete -f statefulsets/
 
 kubectl delete -f deployments/
-kubectl delete -f not_yet/
+#kubectl delete -f not_yet/
 kubectl delete -f secrets/
 kubectl delete -f configmap/
 kubectl delete -f services/
