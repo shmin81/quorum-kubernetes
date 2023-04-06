ebbacebe7b6b3c0b45ab901b419d9c2dc999a68f
sh.min <sh.min@sk.com>
2023-04-07 00:07:17 +0900

    update blockscout deploy

--- playground/kubectl/quorum-besu/ibft2-3node-test/deploy_besu.only.sh
+++ playground/kubectl/quorum-besu/ibft2-3node-test/deploy_besu.only.sh
@@ -2,5 +2,5 @@ kubectl apply -f namespace/
 kubectl apply -f secrets/
 kubectl apply -f configmap/
 kubectl apply -f services/
-kubectl apply -f deployments/
+# kubectl apply -f deployments/
 kubectl apply -f statefulsets/
